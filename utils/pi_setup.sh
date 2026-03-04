e163d73f9b7103cc8f92f720f07085947ee6b4d6
Kristo Isberg <kristo.isberg@gmail.com>
2026-03-04 16:10:43 +0200

    Update setup script

--- utils/pi_setup.sh
+++ utils/pi_setup.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install git python3 python3-pip python3-jupyterlab python3-pandas
+sudo apt install git python3 python3-pip python3-jupyterlab python3-pandas python3-openai
 
 # add these files manually
 chmod 600 .ssh/id_ed25519
@@ -12,4 +12,5 @@ git config --global user.name "Kristo Isberg"
 git clone git@github.com:kristoisberg/masters-thesis.git
 cd masters-thesis
 
+export OPENROUTER_API_KEY=""
 python -m jupyterlab --ip 0.0.0.0 --no-browser --NotebookApp.token='' --NotebookApp.password=''
